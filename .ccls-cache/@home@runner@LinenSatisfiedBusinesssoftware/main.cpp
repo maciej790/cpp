1e2585bfbc32f4a80c8bb4eb1a1ef7873a536e13
maciej790 <60989274+maciej790@users.noreply.github.com>
2023-03-22 11:41:49 +0000

    x

--- .ccls-cache/@home@runner@LinenSatisfiedBusinesssoftware/main.cpp
+++ .ccls-cache/@home@runner@LinenSatisfiedBusinesssoftware/main.cpp
@@ -1,100 +1,65 @@
+////////////////////////////////////////// ZADANIE 1, 2 ///////////////////////////////////
+///////////////////////////////////////// NAZWA: [byleco].cpp ////////////////////////////////////
+
 #include <iostream>
 using namespace std;
 
-class Punkt2D {
-public:
-  string nazwa;
-  int x;
-  int y;
+class Osoba {
+protected:
+  string imie;
+  int wiek;
 
-  void ustawWspolrzedne(int x, int y) {
-    this->x = x;
-    this->y = y;
+public:
+  void ustawWszystko(string imie, int wiek) {
+    this->imie = imie;
+    this->wiek = wiek;
   }
-
-  void ustawNazwe(string nazwa);
-  string zwrocNazwe();
+  void wypiszWszystko();
+  string wypiszImie();
+  void ustawImie(string imie);
+  void ustawWiek(int wiek);
+  int wypiszWiek();
 };
 
-class Kolo : public Punkt2D {
+class Kobieta : public Osoba {
 public:
-  float promien;
-
-  void ustawPromien(float promien) { this->promien = promien; }
-
-  void ustawNazwe(string nazwa);
-  string zwrocNazwe();
-  float zwrocPromien() { return this->promien; }
-  float zwrocObwod() { return 2 * this->promien; }
-  float zwrocPole() { return this->promien * this->promien; }
+  string wypiszImie();
 };
 
-class Punkt3D : public Kolo {
-public:
-  int z;
+class Mezczyzna : public Kobieta {};
 
-  void ustawZ(int z) { this->z = z; }
+void Osoba::wypiszWszystko() {
+  cout << "Imie: " << imie << endl << "Wiek: " << wiek << endl;
+}
 
-  void ustawNazwe(string nazwa);
-  string zwrocNazwe();
-};
-
-class Kula : public Punkt3D {
-public:
-  void ustawNazwe(string nazwa);
-  string zwrocNazwe();
-
-  float zwrocPoleKuli() { return 4 * (this->promien * this->promien); }
-  float zwrocObjetoscKuli() {
-    return (4 / 3) * (this->promien * this->promien * this->promien);
-  }
-
-  void zwrocWspolrzedne() {
-    cout << this->x << endl;
-    cout << this->y << endl;
-    cout << this->z << endl;
-  }
-};
-
-void Punkt2D::ustawNazwe(string nazwa) { this->nazwa = nazwa; };
-string Punkt2D::zwrocNazwe() { return this->nazwa; }
-
-void Punkt3D::ustawNazwe(string nazwa) { this->nazwa = nazwa; };
-string Punkt3D::zwrocNazwe() { return this->nazwa; }
-
-void Kolo::ustawNazwe(string nazwa) { this->nazwa = nazwa; };
-string Kolo::zwrocNazwe() { return this->nazwa; }
-
-void Kula::ustawNazwe(string nazwa) { this->nazwa = nazwa; };
-string Kula::zwrocNazwe() { return this->nazwa; }
+string Kobieta::wypiszImie() { return this->imie; }
+string Osoba::wypiszImie() { return this->imie; }
+void Osoba::ustawImie(string imie) { this->imie = imie; }
+void Osoba::ustawWiek(int wiek) { this->wiek = wiek; }
+int Osoba::wypiszWiek() { return this->wiek; }
 
 int main() {
-  Punkt2D punkt2D;
-  punkt2D.ustawWspolrzedne(1, 2);
-  punkt2D.ustawNazwe("punkt_2D");
-  cout << punkt2D.zwrocNazwe() << endl;
-
-  Punkt3D punkt3D;
-  punkt3D.ustawZ(3);
-  punkt3D.ustawNazwe("punkt_3D");
-  cout << punkt3D.zwrocNazwe() << endl;
-
-  Kolo kolo;
-  kolo.ustawPromien(5);
-  kolo.ustawNazwe("kolo");
-  cout << kolo.zwrocNazwe() << endl;
-  cout << kolo.zwrocPromien() << endl;
-  cout << kolo.zwrocObwod() << " PI" << endl;
-  cout << kolo.zwrocPole() << " PI" << endl;
-
-  Kula kula;
-  kula.ustawNazwe("kula");
-  kula.ustawWspolrzedne(6, 7);
-  kula.ustawZ(8);
-  kula.ustawPromien(9);
-  cout << kula.zwrocNazwe() << endl;
-  cout << kula.zwrocPromien() << endl;
-  cout << kula.zwrocPoleKuli() << " PI" << endl;
-  cout << kula.zwrocObjetoscKuli() << " PI" << endl;
-  kula.zwrocWspolrzedne();
+  string imie;
+  int wiek;
+  Osoba Karol;
+  cin >> imie;
+  cin >> wiek;
+  Karol.ustawWszystko(imie, wiek);
+  Karol.wypiszWszystko();
+  Kobieta Alicja;
+  cin >> imie;
+  cin >> wiek;
+  Alicja.ustawWszystko(imie, wiek);
+  Alicja.wypiszWszystko();
+  cout << Alicja.wypiszImie() << endl;
+  Mezczyzna mezczyzna;
+  cin >> imie;
+  cin >> wiek;
+  mezczyzna.ustawWszystko(imie, wiek);
+  mezczyzna.wypiszWszystko();
+  cout << mezczyzna.wypiszImie() << endl;
+  return 0;
 }
+
+////////////////////////////////////////// ZADANIE 1, 2 ///////////////////////////////////
+///////////////////////////////////////// NAZWA: [byleco].cpp ////////////////////////////////////
